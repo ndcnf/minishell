99e64ad1f46070151fda0ed8dcc80eac4581b81c
ndcnf <chat.cheshire@gmail.com>
2022-07-26 11:34:17 +0200

    added -n fix

--- src/builtins.c
+++ src/builtins.c
@@ -6,7 +6,7 @@
 /*   By: nchennaf <nchennaf@student.42lausanne.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/24 16:40:41 by nchennaf          #+#    #+#             */
-/*   Updated: 2022/07/25 17:56:06 by nchennaf         ###   ########.fr       */
+/*   Updated: 2022/07/26 11:32:07 by nchennaf         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -58,7 +58,6 @@ int	b_echo(t_builtins *bs)
 					if (i != bs->n_args)
 						printf(" ");
 				}
-				printf("\n");
 			}
 		}
 		else
@@ -71,6 +70,7 @@ int	b_echo(t_builtins *bs)
 				if (i != bs->n_args)
 					printf(" ");
 			}
+			printf("\n");
 		}
 	}
 	return (EXIT_SUCCESS);
