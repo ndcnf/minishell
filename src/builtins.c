b88d04b3b384673c37f1cd7df0d25fce942e1773
ndcnf <chat.cheshire@gmail.com>
2022-07-26 14:59:55 +0200

    b_pwd norm OK

--- src/builtins.c
+++ src/builtins.c
@@ -6,7 +6,7 @@
 /*   By: nchennaf <nchennaf@student.42lausanne.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/24 16:40:41 by nchennaf          #+#    #+#             */
-/*   Updated: 2022/07/26 14:47:07 by nchennaf         ###   ########.fr       */
+/*   Updated: 2022/07/26 14:55:17 by nchennaf         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,9 +44,9 @@ int	b_echo(t_builtins *bs)
 {
 	int	i;
 
-	if (bs->n_args == 1) //si juste 'echo'
+	if (bs->n_args == 1)
 		printf("\n");
-	if (bs->n_args >= 2) //'echo' et ses complements
+	if (bs->n_args >= 2)
 	{
 		if (ft_strncmp(bs->args[1], "-n", strlen(bs->args[1])) == 0)
 		{
@@ -80,13 +80,9 @@ int	b_echo(t_builtins *bs)
 
 int	b_pwd(t_builtins *bs)
 {
-	// utiliser le path de la fonction pwd directement
-	// fonction getcwd() a utiliser
-
 	if (getcwd(bs->path, sizeof(bs->path)) == NULL)
-		perror("getcwd() error");
+		perror("error");
 	else
 		printf("%s\n", bs->path);
-
 	return (EXIT_SUCCESS);
 }
