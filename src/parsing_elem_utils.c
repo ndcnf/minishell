fefff60e1bc9596ab71aa889c6e9580e2836bb42
chocogruh <mthiesso@student.42lausanne.ch>
2022-09-22 16:53:14 +0200

    parsing_detail

--- src/parsing_elem_utils.c
+++ src/parsing_elem_utils.c
@@ -6,32 +6,32 @@
 /*   By: mthiesso <mthiesso@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/22 16:08:20 by mthiesso          #+#    #+#             */
-/*   Updated: 2022/09/22 16:09:22 by mthiesso         ###   ########.fr       */
+/*   Updated: 2022/09/22 16:53:04 by mthiesso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	malloc_elem()
-{
-	if (s[i] == '\"' || s[i] == '\'')
-			j = (is_quotes(s, i) + 1);
-	else
-	{
-		while (s[i])
-		{
-			if (s[i] == '\"' || s[i] == '\'')
-			{
-				j = (is_quotes(s, i) + 1);
-				i = j;
-			}
-			if (!s[i])
-				break ;
-			if (s[i] == ' ')
-				break ;
-			i++;
-			j++;
-		}
-	}
-	in->elem->cont[n] = malloc(sizeof(char) * j + 1);
-}
+// void	malloc_elem()
+// {
+// 	if (s[i] == '\"' || s[i] == '\'')
+// 			j = (is_quotes(s, i) + 1);
+// 	else
+// 	{
+// 		while (s[i])
+// 		{
+// 			if (s[i] == '\"' || s[i] == '\'')
+// 			{
+// 				j = (is_quotes(s, i) + 1);
+// 				i = j;
+// 			}
+// 			if (!s[i])
+// 				break ;
+// 			if (s[i] == ' ')
+// 				break ;
+// 			i++;
+// 			j++;
+// 		}
+// 	}
+// 	in->elem->cont[n] = malloc(sizeof(char) * j + 1);
+// }
