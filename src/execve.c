b1f82e6824101f93cc9a7d4fc9d9556344d50ff0
chocogruh <mthiesso@student.42lausanne.ch>
2022-10-04 19:33:56 +0200

    absolute_path OK

--- src/execve.c
+++ src/execve.c
@@ -6,7 +6,7 @@
 /*   By: mthiesso <mthiesso@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/21 17:05:55 by mthiesso          #+#    #+#             */
-/*   Updated: 2022/10/04 18:10:50 by mthiesso         ###   ########.fr       */
+/*   Updated: 2022/10/04 19:31:35 by mthiesso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,15 +18,18 @@ void	exec_boarders(t_data *dt, int in)
 	int			ok;
 
 	ok = 0;
-	i = where_in_env(dt, "PATH", 4);
-	if (i == NO_RESULT)
+	if (on_my_way(dt, ok, dt->in[in].elem->cont[0], in) != 1)
 	{
-		the_end(CMD_404, EXIT_FAILURE, 1);
-		return ;
+		i = where_in_env(dt, "PATH", 4);
+		if (i == NO_RESULT)
+		{
+			the_end(CMD_404, EXIT_FAILURE, 1);
+			return ;
+		}
+		ok = exec_middle(dt, in, ok, i);
+		if (ok == 0)
+			the_end(CMD_404, EXIT_FAILURE, 1);
 	}
-	ok = exec_middle(dt, in, ok, i);
-	if (ok == 0)
-		the_end(CMD_404, EXIT_FAILURE, 1);
 	while (in < dt->n_cmd)
 	{
 		waitpid(dt->in[in++].pid, &g_exit_stat, 0);
